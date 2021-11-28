5a8b9c19953ee08af80d8f2b51ff002f70d9f9c9
Ayush Kulshrestha <ayushkulshrestha2001@gmail.com>
2021-11-28 22:12:18 +0530

    completed project

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,33 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  final int height;
+  final int weight;
+  double _bmi;
+  CalculatorBrain({this.height, this.weight});
+
+  String calculateBMI() {
+    _bmi = weight / pow(height / 100, 2);
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'Try to exercise more.';
+    } else if (_bmi > 18.5) {
+      return 'Good Job. Keep going!';
+    } else {
+      return 'Try to gain weight.';
+    }
+  }
+}
